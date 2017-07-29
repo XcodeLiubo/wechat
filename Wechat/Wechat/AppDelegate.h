c3d60afab7861ca13bc1afe789b5f63843b55c7a
liubo <liubo@163.com>
2017-07-29 11:04:10 +0800

    创建了一个新项目

---
+++ Wechat/Wechat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Wechat
+//
+//  Created by   LiuBo on 2017/7/29.
+//  Copyright © 2017年 LiuBo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Wechat/Wechat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Wechat
+//
+//  Created by   LiuBo on 2017/7/29.
+//  Copyright © 2017年 LiuBo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
